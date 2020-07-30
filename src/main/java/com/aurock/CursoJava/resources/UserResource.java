34b2e3d51e8739491eefab3c3407ddb6702a6e86
AuRocKbra <4haurock@gamil.com>
2020-07-30 17:19:17 -0300

    Implementado metodo rest para delete de usuário

--- src/main/java/com/aurock/CursoJava/resources/UserResource.java
+++ src/main/java/com/aurock/CursoJava/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,7 +43,16 @@ public class UserResource {
 	 * */
 	public ResponseEntity<User>insert(@RequestBody User obj){
 		obj = service.insert(obj);
+		/*
+		 * O atributo abaixo ira gerar um url como resposta a criação do registro. Com esta url é posível acessar os dados do usuário;
+		 * */
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value="/{id}")//parametro para deletar um registro
+	public ResponseEntity<Void>delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
