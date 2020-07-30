86f45fb917c01f99f5c95b9285fe39b06a61ac03
AuRocKbra <4haurock@gamil.com>
2020-07-30 16:57:00 -0300

    Implementado metodo request para insert de usuarios

--- src/main/java/com/aurock/CursoJava/resources/UserResource.java
+++ src/main/java/com/aurock/CursoJava/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.aurock.CursoJava.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.aurock.CursoJava.entities.User;
 import com.aurock.CursoJava.services.UserService;
@@ -29,5 +33,16 @@ public class UserResource {
 	public ResponseEntity<User>findById(@PathVariable Long id){
 		User objUser = service.findById(id);
 		return ResponseEntity.ok().body(objUser);
-	} 
+	}
+	
+	@PostMapping //parametro do Json que defino o uso do metodo Post do http, para enviar dados
+	/*
+	 * Como se trata de uma inserção de dados o correto é retornar o status 201, que no http siginifica a criação de um novo recurso
+	 * para isso é necessário chamar o metodo created do Response o qual prescisa de uma URI para ser executada;
+	 * */
+	public ResponseEntity<User>insert(@RequestBody User obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
