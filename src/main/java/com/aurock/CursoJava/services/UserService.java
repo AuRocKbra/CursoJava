c74dfc58a8fb46140062da8801af51289919bce6
AuRocKbra <4haurock@gamil.com>
2020-07-30 17:40:30 -0300

    Implementado metodo de update do usuário

--- src/main/java/com/aurock/CursoJava/services/UserService.java
+++ src/main/java/com/aurock/CursoJava/services/UserService.java
@@ -31,4 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id); 
 	}
+	
+	public User update (Long id, User obj) {
+		User entity = repository.getOne(id);//Sem consultar o banco, a variável armazenará os dados para alteração do registro informado
+		updateData(entity,obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
